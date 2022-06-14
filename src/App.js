33c09b51bd40a34bc966ab52b51f72e195d913fd
Reshma Shaikh <reshma.shaikh7707@gmail.com>
2022-06-14 23:09:55 +0530

    menucard project completed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Restaurant from './component/basics/Restaurant'
+import UseState from './component/Hooks/useState'
+import UseEffect from './component/Hooks/useEffect'
+import UseReducer from './component/Hooks/useReducer'
+const App = () => {
+   
+  
+  // return <Restaurant  />
+  // return <UseState />
+  // return <UseEffect />
+  return <UseReducer />
+  
+}
+
+export default App;
+
+
